896886709d35fdba90a7f3a21a7d585ed4ec2817
Kelvin Souza <kelvinsouza2017@hotmail.com>
2023-08-20 19:35:56 -0300

    Criação de um segundo obj 'cliente'

--- src/java_training/TesteCliente.java
+++ src/java_training/TesteCliente.java
@@ -5,13 +5,21 @@ public class TesteCliente {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Cliente objCliente = new Cliente();
-		objCliente.agencia = 1234;
-		objCliente.contaCorrente = "0123-4";
-		objCliente.cpf = "012.456.789-10";
-		objCliente.saldoContaCorrente = 1000.00f;
+		Cliente clienteA = new Cliente();
+		clienteA.agencia = 1234;
+		clienteA.contaCorrente = "0123-4";
+		clienteA.cpf = "012.456.789-10";
+		clienteA.saldoContaCorrente = 1000.00f;
 		
-		System.out.print(objCliente);
+		System.out.print(clienteA);
+		
+		Cliente clienteB = new Cliente();
+		clienteB.agencia = 5678;
+		clienteB.contaCorrente = "5647-8";
+		clienteB.cpf = "023.567.891-00";
+		clienteB.saldoContaCorrente = 2000.00f;
+		
+		System.out.print(clienteB);
 		
 		System.out.println("Projeto - Orientação a Objetos...");
 	}
