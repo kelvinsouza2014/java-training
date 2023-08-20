10017879e4f2e4b04c0897da08dc265220420ea4
Kelvin Souza <kelvinsouza2017@hotmail.com>
2023-08-20 19:13:13 -0300

    Instancia da classe 'Cliente'

---
+++ src/java_training/TesteCliente.java
@@ -0,0 +1,13 @@
+package java_training;
+
+public class TesteCliente {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Cliente objCliente = new Cliente();
+		
+		System.out.println("Projeto - Orientação a Objetos...");
+	}
+
+}
