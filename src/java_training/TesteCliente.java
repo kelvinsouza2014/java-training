2dcb9b8205d47125661c148bebeaf7ad6987b9f3
Kelvin Souza <kelvinsouza2017@hotmail.com>
2023-08-20 21:46:56 -0300

    Correção de bug e finalização

--- src/java_training/TesteCliente.java
+++ src/java_training/TesteCliente.java
@@ -6,6 +6,7 @@ public class TesteCliente {
 		// TODO Auto-generated method stub
 
 		Cliente clienteA = new Cliente();
+		
 		clienteA.agencia = 1234;
 		clienteA.contaCorrente = "0123-4";
 		clienteA.cpf = "012.456.789-10";
@@ -13,17 +14,23 @@ public class TesteCliente {
 		
 		clienteA.sacarContaCorrente(500.00f);
 		
-		System.out.print(clienteA);
+		String agenciaConta = clienteA.informarAgenciaConta();
 		
-		Cliente clienteB = new Cliente();
+		System.out.print(clienteA);
+		System.out.print("Agencia e conta do fulano é: " + agenciaConta);
+		
+		Cliente clienteB = new Cliente(); 
 		clienteB.agencia = 5678;
 		clienteB.contaCorrente = "5647-8";
 		clienteB.cpf = "023.567.891-01";
 		clienteB.saldoContaCorrente = 2000.00f;
 		
-		clienteA.sacarContaCorrente(1000.00f);
+		clienteB.sacarContaCorrente(1000.00f);
+
+		String agenciaContaB = clienteB.informarAgenciaConta();
 		
 		System.out.print(clienteB);
+		System.out.print("Agencia e conta do fulano é: " + agenciaContaB);
 		
 		System.out.println("Projeto - Orientação a Objetos...");
 	}
